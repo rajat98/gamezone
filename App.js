5f5f6e7ac8028a0e42a525ac0dcbd819adfec15a
rajat <rajat@kali.rpaw.org>
2021-05-23 01:55:52 +0530

    custom font

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './screens/home'
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/font/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/font/Nunito-Bold.ttf')
+  });
+
 export default function App() {
-  return (
-    <Home/>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if(fontsLoaded) {
+    return (
+      <Home/>
+      );
+  }else{
+    return(
+        <AppLoading
+            startAsync={getFonts}
+            onFinish={()=>setFontsLoaded(true)}
+            onError={console.warn}
+        />
+    )
+  }
 }
 
 
--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
-export default function home(){
+export default function Home(){
     return(
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     )
 }
@@ -13,4 +13,8 @@ const styles =StyleSheet.create({
     container:{
         padding:24,
     },
+    titleText:{
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+    }
 })
