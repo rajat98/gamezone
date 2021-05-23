b2bee55696ce91464f7f0226b590321074faf867
rajat <rajat@kali.rpaw.org>
2021-05-23 15:50:32 +0530

    global styles

---
+++ styles/global.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container:{
+        padding:40,
+    },
+    titleText:{
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+    },
+    paragraph:{
+        marginVertical: 8,
+        lineHeight: 18,
+    }
+})
