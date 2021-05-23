3f2152b99573d703f81a07080a0eb18b74d254b4
rajat <rajat@kali.rpaw.org>
2021-05-24 00:21:44 +0530

    custom header

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,8 +1,15 @@
+import React from "react";
 import {createStackNavigator} from "react-navigation-stack";
-import About from '../screens/about';
+import Header from '../shared/header';
+import About from '../screens/about'
 const screens = {
     About:{
         screen: About,
+        navigationOptions:({navigation})=>{
+            return({
+                headerTitle: () => <Header navigation={navigation} title='About GameZone'/>,
+            })
+        }
     },
 }
 // use default navigation here instead of declaring for each screen
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,10 +1,17 @@
+import React from "react";
 import {createStackNavigator} from "react-navigation-stack";
-import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewdetail';
+import Header from '../shared/header'
+import Home from "../screens/home";
 const screens = {
     //home screen will be default as it is on top
     Home:{
         screen: Home,
+        navigationOptions: ({navigation}) => {
+            return({
+                headerTitle: () => <Header navigation={navigation} title={'GameZone'}/>
+            })
+        },
     },
     ReviewDetails:{
         screen: ReviewDetails,
