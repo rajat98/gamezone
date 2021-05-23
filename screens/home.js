3f43e3217aa7a36ddfc12187ac45fec8289bf148
rajat <rajat@kali.rpaw.org>
2021-05-23 16:33:19 +0530

    Navigating around

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {Button, View, Text} from 'react-native';
 import {globalStyles} from "../styles/global";
 
-export default function Home(){
+export default function Home({navigation} ){
+    //When we configure Navigator every screen gets navigation property assigned as props
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+        //will do same thing more explicit though
+        // navigation.push('ReviewDetails');
+    }
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='Review Details' onPress={pressHandler}></Button>
         </View>
     )
 }
--- screens/reviewdetail.js
+++ screens/reviewdetail.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { View, Text, Button} from 'react-native';
 import {globalStyles} from "../styles/global";
 
-export default function home(){
+export default function home({navigation}){
+    const pressHandler = () => {
+        //goBack to last screen implicitly pops
+        navigation.goBack();
+        //explicit pop function
+        // navigation.pop();
+    }
     return(
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Button title='back' onPress={pressHandler}/>
         </View>
     )
 }
